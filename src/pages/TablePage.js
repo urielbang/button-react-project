a930356b6af95781e5a9d699540eeeb8916e057d
urielbang <uri32655@gmail.com>
2023-12-11 15:06:48 +0200

    Fragment component

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -12,7 +12,11 @@ function TablePage() {
       label: "Color",
       render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`} />,
     },
-    { label: "Score", render: (fruit) => fruit.score },
+    {
+      label: "Score",
+      render: (fruit) => fruit.score,
+      header: () => <th className="bg-red-500">Score</th>,
+    },
   ];
   const keyFn = (fruit) => {
     return fruit.name;
